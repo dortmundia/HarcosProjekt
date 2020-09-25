1ffa11da50d9fbfed5f13cc41048f7fb620adf04
dortmundia <csulienjatek@gmail.com>
2020-09-25 11:16:22 +0200

    harcos konstruktor, harcos adatainak megadása

--- HarcosProjekt/Program.cs
+++ HarcosProjekt/Program.cs
@@ -11,12 +11,46 @@ namespace HarcosProjekt
         private string nev;
         private int lvl;
         private int hp;
+        private int xp;
         private int    basHP;
         private int basDMG;
 
+        public Harcos(string nev, int statuszSablon)
+        {
+            this.nev = nev;
 
+            this.lvl = 1;
+            this.xp = 0;
+            //this.hp = hp;
 
+            if (statuszSablon == 1)
+            {
+                this.basHP = 15;
+                this.basDMG = 3;
+            }
+            else if (statuszSablon == 2)
+            {
+                this.basHP = 12;
+                this.basDMG = 4;
+            }
+            else if (statuszSablon == 3)
+            {
+                this.basHP = 8;
+                this.basDMG = 5;
+            }
+            else
+            {
+                Console.WriteLine("rossz statuszSablont adot meg");
+            }
+            //this.hp = this.basHP + this.lvl * 3;
+        }
+
+            public int MAXhp { get => this.basHP + this.lvl * 3; }
+        
+    
+    
     }
+
 
 
     class Program
@@ -29,7 +63,7 @@ namespace HarcosProjekt
 
 
 
-
+        Console.ReadKey();
         }
     }
 }
